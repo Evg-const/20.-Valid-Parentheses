67df8b8513be619feb7b27694e4ee580e0cd3351
Евгений Константинов <kanstantsinav@gmail.com>
2021-10-22 20:31:49 +0300

    Добавьте файлы проекта.

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Valid_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из скобок (){}[]");
+
+            string str = Console.ReadLine();
+
+            Console.WriteLine(IsValid(str));
+
+            Console.Read();
+
+
+            bool IsValid(string s)
+            {
+
+                Stack stack = new Stack();          //Создаем стек
+
+                foreach (char c in s)               //перебираем 
+                {
+
+                    switch (c)                      //Принимаем первый символ (и открывающие скобки)
+                    {
+                        case '(':
+                            stack.Push(c);          //Пихаем в стек
+                            break;
+                        case '{':
+                            stack.Push(c);
+                            break;
+                        case '[':
+                            stack.Push(c);
+                            break;
+                    }
+
+                    if (stack.Count == 0) return false;     //Если в первом символе нет открывающихся скобок - возвращаем falce
+
+                    switch (c)
+                    {
+                        case ')':
+                            if (stack.Peek().ToString() == "(") stack.Pop();    //Если пришла закрывающаяся скабка, проверяем её в стеке, если подходит, удаляем
+                            else return false;                                  //Если нет, возвращаем falce
+                            break;
+                        case '}':
+                            if (stack.Peek().ToString() == "{") stack.Pop();
+                            else return false;
+                            break;
+                        case ']':
+                            if (stack.Peek().ToString() == "[") stack.Pop();
+                            else return false;
+                            break;
+                    }
+
+                }
+                if (stack.Count == 0) return true;                         //Если после всего цикла стек пустой, значит все скобки закрылись возвращаем true
+                else return false;                                          // Если нет - Falce
+
+            }
+
+        }
+    }
+}
